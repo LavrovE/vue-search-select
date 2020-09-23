db378b270355c0c33e2880d9cd7755e8a3d7c074
eugene <jenya@mylearninghub.com>
2020-09-23 17:08:25 +0300

    single select

--- src/lib/BasicSelect.vue
+++ src/lib/BasicSelect.vue
@@ -6,7 +6,7 @@
     @click="openOptions"
   >
     <div class="iconSlot">
-      <i class="delete icon" @click="deleteTextOrItem"></i>
+      <i class="delete icon" @click.prevent="deleteTextOrItem"></i>
       <div class="toggleButton" style="cursor: pointer; margin-left: auto;" @click.stop="toggleOptions">
         <slot name="icon"></slot>
       </div>
